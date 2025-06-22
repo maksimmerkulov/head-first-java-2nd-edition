4af9662cb0573ad9ae7c21cd746edea0a956a99a
maksimmerkulov <merkulov.mv@outlook.com>
2025-06-22 21:58:01 +0500

    Chapter 2: BE the compiler (A Trip to Objectville)

---
+++ src/main/java/com/headfirstjava/chapter02/DVDPlayerTestDrive.java
@@ -0,0 +1,12 @@
+package com.headfirstjava.chapter02;
+
+public class DVDPlayerTestDrive {
+    public static void main(String[] args) {
+        DVDPlayer d = new DVDPlayer();
+        d.canRecord = true;
+        d.playDVD();
+        if (d.canRecord) {
+            d.recordDVD();
+        }
+    }
+}
