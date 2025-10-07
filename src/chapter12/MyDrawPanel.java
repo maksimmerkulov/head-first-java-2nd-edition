c8a9f8fc38ca5d7cf31dad9ff18834484146e95d
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-07 14:31:47 +0500

    Chapter 12: Because life’s too short to paint the circle a solid color when there’s a gradient blend waiting for you. (A Very Graphic Story)

--- src/chapter12/MyDrawPanel.java
+++ src/chapter12/MyDrawPanel.java
@@ -8,8 +8,17 @@ class MyDrawPanel extends JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
-        GradientPaint gradient = new GradientPaint(70,70,Color.blue, 150,150, Color.orange);
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+        Color startColor = new Color(red, green, blue);
 
+        red = (int) (Math.random() * 256);
+        green = (int) (Math.random() * 256);
+        blue = (int) (Math.random() * 256);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(70,70,startColor, 150,150, endColor);
         g2d.setPaint(gradient);
         g2d.fillOval(70,70,100,100);
     }
