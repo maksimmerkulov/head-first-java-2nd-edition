5f52c9377b5248adb3fced1da957c7b28d75eb57
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-11 16:21:58 +0500

    Chapter 13: The Big Three layout managers: border, flow, and box. (Work on Your Swing)

--- src/chapter13/Panel1.java
+++ src/chapter13/Panel1.java
@@ -14,8 +14,13 @@ public class Panel1 {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray);
+
+        JButton button = new JButton("shock me");
+
+        panel.add(button);
         frame.getContentPane().add(BorderLayout.EAST, panel);
-        frame.setSize(200,200);
+
+        frame.setSize(250,200);
         frame.setVisible(true);
     }
 }
